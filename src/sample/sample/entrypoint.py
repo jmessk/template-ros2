a690da1eb2f4879432db021abe7334688c05df05
jme-rs <jme.co.jp@gmail.com>
2024-08-13 14:13:01 +0900

    feat: Add sample node and entrypoint script
    The code changes add a new sample node and an entrypoint script to the project. The sample node is a ROS2 node that creates a subscription to the topic "/sample" and logs the received message. The entrypoint script initializes the ROS2 node and handles the shutdown process.
    
    Based on the recent user commits and repository commits, it seems that the developer is working on adding functionality related to ROS2.

---
+++ src/sample/sample/entrypoint.py
@@ -0,0 +1,21 @@
+import rclpy
+import cv2
+import os
+
+from .sample_node import SampleNode
+
+
+def main():
+    try:
+        rclpy.init()
+        sample_node = SampleNode()
+        rclpy.spin(sample_node)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        cv2.destroyAllWindows()
+        sample_node.destroy_node()
+        rclpy.shutdown()
+        print("Display node shutdown")
